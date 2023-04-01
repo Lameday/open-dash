67204a6ddc08670964abb23f319625ff86385ef9
Norbert Satro <norbert.satro@3s.pl>
2023-04-01 18:43:30 +0200

    Added persist UserData in session storage

--- src/store/userStore.ts
+++ src/store/userStore.ts
@@ -1,12 +1,21 @@
 import { create } from 'zustand';
 import { UserData } from '../domains/SignIn/model/UserData';
+import { persist, createJSONStorage } from 'zustand/middleware';
 
 interface UserState {
     userData: UserData | null;
     setUser: (userData: UserData) => void;
 }
 
-export const useUserStore = create<UserState>((set) => ({
-    userData: null,
-    setUser: (userData) => set(() => ({ userData: userData })),
-}));
+export const useUserStore = create<UserState>()(
+    persist(
+        (set) => ({
+            userData: null,
+            setUser: (userData) => set(() => ({ userData: userData })),
+        }),
+        {
+            name: 'loggedUser',
+            storage: createJSONStorage(() => sessionStorage),
+        },
+    ),
+);
