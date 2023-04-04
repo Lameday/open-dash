957af2a117285d4a7afe445e4479660d8073504b
Lameday <thelameday@gmail.com>
2023-04-04 16:19:35 +0200

    refactor: adjusting code to changes from main

--- src/translations/i18n.ts
+++ src/translations/i18n.ts
@@ -4,9 +4,11 @@ import { initReactI18next } from 'react-i18next';
 //EN Translations
 import dashboardTranslations from './en/dashboard/dashboard.json';
 import noAuthTranslations from './en/noAuth/noAuth.json';
+import moduleOneTranslations from './en/moduleOne/moduleOne.json';
 
 //PL Translations
 import dashboardTranslationsPL from './pl/dashboard/dashboard.json';
+import moduleOneTranslationsPL from './pl/moduleOne/moduleOne.json';
 import noAuthTranslationsPL from './pl/noAuth/noAuth.json';
 
 const resources = {
@@ -14,26 +16,25 @@ const resources = {
         translation: {
             ...dashboardTranslations,
             ...noAuthTranslations,
+            ...moduleOneTranslations,
         },
     },
     pl: {
         translation: {
             ...dashboardTranslationsPL,
             ...noAuthTranslations,
+            ...moduleOneTranslationsPL,
         },
     },
 };
 
-i18n.use(initReactI18next) // passes i18n down to react-i18next
-    .init({
-        resources,
-        lng: 'en', // language to use, more information here: https://www.i18next.com/overview/configuration-options#languages-namespaces-resources
-        // you can use the i18n.changeLanguage function to change the language manually: https://www.i18next.com/overview/api#changelanguage
-        // if you're using a language detector, do not define the lng option
+i18n.use(initReactI18next).init({
+    resources,
+    lng: 'en',
 
-        interpolation: {
-            escapeValue: false, // react already safes from xss
-        },
-    });
+    interpolation: {
+        escapeValue: false,
+    },
+});
 
 export default i18n;
